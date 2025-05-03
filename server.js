0d16d69fd4a252473c8fba98e237eeb54693ad29
bibek1414 <bbhattarai770@gmail.com>
2025-05-03 17:55:09 +0545

    email to mail

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const transporter = nodemailer.createTransport({
 });
 
 // Email sending endpoint
-app.post('/api/send-email', async (req, res) => {
+app.post('/api/send-mail', async (req, res) => {
   const { firstName, lastName, email, phone, message } = req.body;
   
   if (!firstName || !lastName || !email || !message) {
